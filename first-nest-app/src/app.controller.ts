9f3aef2577986de85e7024655efd81434f6eee7b
Muhammad Musa Mueed <musavuswh@gmail.com>
2024-12-21 00:39:04 +0500

    learned about route parameters in nestjs

--- first-nest-app/src/app.controller.ts
+++ first-nest-app/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AnswerDto } from './dto/app.dto';
 
@@ -6,20 +6,32 @@ import { AnswerDto } from './dto/app.dto';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // Root Route - endpoint
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
+  // Customized GET route
   @Get('/askquestion')
   askQuestion(): string {
     return "How are you?";
   }
 
+  // Using Body data as DTO - POST request
   @Post('/answer')
   answer(@Body() getAnswerDto: AnswerDto): object {
     return {
       answer: getAnswerDto.answer,
+    }
+  }
+
+
+  // We have to define routes - using Route Parameters - at the end of controllers
+  @Get(':id')
+  getRouteParam(@Param('id') userId): object {
+    return {
+      userId: userId, 
     }
   }
 }
