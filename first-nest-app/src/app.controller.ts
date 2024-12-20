800f972eb6d6e58a5f1acc130852c1cf3f5c775c
Muhammad Musa Mueed <musavuswh@gmail.com>
2024-12-21 00:24:43 +0500

    Learned about controllers & dto in nestjs

--- first-nest-app/src/app.controller.ts
+++ first-nest-app/src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AnswerDto } from './dto/app.dto';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,17 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/askquestion')
+  askQuestion(): string {
+    return "How are you?";
+  }
+
+  @Post('/answer')
+  answer(@Body() getAnswerDto: AnswerDto): object {
+    return {
+      answer: getAnswerDto.answer,
+    }
   }
 }
